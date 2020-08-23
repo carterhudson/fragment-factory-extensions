04a8da1130f8ca612638008aee439dce412e26e9
Carter Hudson <saganaut@gmail.com>
2020-08-23 16:02:13 -0600

    add overloads for intermediate types

--- lib/src/main/java/com/carterhudson/FragmentFactoryExtensions.kt
+++ lib/src/main/java/com/carterhudson/FragmentFactoryExtensions.kt
@@ -9,12 +9,20 @@ inline fun <reified TypeT> Any.cast(): TypeT = this as TypeT
 
 inline fun <reified TypeT> nameOf(): String = TypeT::class.java.name
 
-inline fun <reified FragmentT : Fragment> FragmentFactory.instantiate(arguments: Bundle): FragmentT =
+inline fun <reified FragmentT : Fragment> FragmentFactory.instantiate(args: Bundle): FragmentT =
   instantiate(ClassLoader.getSystemClassLoader(), nameOf<FragmentT>())
     .cast<FragmentT>()
-    .apply { setArguments(arguments) }
+    .apply { arguments = args }
 
 inline fun <reified FragmentT : Fragment> FragmentFactory.instantiate(vararg args: Pair<String, Any?>): FragmentT =
-  instantiate(ClassLoader.getSystemClassLoader(), nameOf<FragmentT>())
-    .cast<FragmentT>()
-    .apply { arguments = bundleOf(*args) }
+  instantiate(bundleOf(*args))
+
+inline fun <reified FragmentT : Fragment> FragmentFactory.instantiate(args: List<Pair<String, Any?>>): FragmentT =
+  args
+    .toTypedArray()
+    .let { instantiate(*it) }
+
+inline fun <reified FragmentT : Fragment> FragmentFactory.instantiate(args: Map<String, Any?>): FragmentT =
+  args
+    .toList()
+    .let(::instantiate)
